4563047beda1a9df2fae1c9ed4cfa4a53aa500cc
Mehdi Baneshi <baneshi@gmail.com>
2025-06-07 01:23:51 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,9 @@
+import React from 'react'
+import HabitReminderSystem from './HabitReminderSystem'
+import './index.css'
 
+function App() {
+  return <HabitReminderSystem />
+}
+
+export default App
